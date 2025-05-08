72c377acb96c888e2d33366da2e4acc464add5a1
simnple <75520229+simnple@users.noreply.github.com>
2025-05-08 22:28:05 +0900

    Update and rename Main.py to main.py

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 import zlib
+import sys
 
-customBase85 = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%()*+,-./:;=?@[]^_{}'
+custom_base85_chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%()*+,-./:;=?@[]^_{}"
 
-def Base85ToBin(inputStringBase85):
-    global customBase85
-
-    size = len(inputStringBase85)
+def base85_to_bin(bin_value: str):
+    size = len(bin_value)
     bin_data = bytearray()
 
     i = 0
     while i < size:
-        a = (customBase85.index(inputStringBase85[i]) * 85**4)
+        a = (custom_base85_chars.index(bin_value[i]) * 85**4)
         i += 1
 
         if i < size:
-            a += (customBase85.index(inputStringBase85[i]) * 85**3)
+            a += (custom_base85_chars.index(bin_value[i]) * 85**3)
             i += 1
 
         if i < size:
-            a += (customBase85.index(inputStringBase85[i]) * 85**2)
+            a += (custom_base85_chars.index(bin_value[i]) * 85**2)
             i += 1
 
         if i < size:
-            a += (customBase85.index(inputStringBase85[i]) * 85)
+            a += (custom_base85_chars.index(bin_value[i]) * 85)
             i += 1
 
         if i < size:
-            a += customBase85.index(inputStringBase85[i])
+            a += custom_base85_chars.index(bin_value[i])
             i += 1
 
             bin_data.extend([
@@ -61,14 +60,9 @@ def Base85ToBin(inputStringBase85):
     return bin_data
 
 if __name__ == "__main__":
+    bin_value = open(sys.argv[1], "r").read()
 
-    inputStringBase85 = input("Input the text > ")
-
-    bin_data = Base85ToBin(inputStringBase85)
-
+    bin_data = base85_to_bin(bin_value)
     decompressed_data = zlib.decompress(bin_data)
 
-    with open("output.lua", 'wb') as f:
-        f.write(decompressed_data)
-
-    print("Done.")
+    open("result.lua", "wb").write(decompressed_data)
